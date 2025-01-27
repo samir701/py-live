bc6fa4e96fc202a9af994c56921c3a52fb7738f3
samir701 <84538095+samir701@users.noreply.github.com>
2025-01-27 23:43:08 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,23 @@
+# print("hwllow world")
+#int,char,,float
+#int->integers
+#float->decimal
+#char->string
+var=10
+char="hellow"
+var1=20
+print(type(var))
+print(type(char))
+print(type(var1))
+
+#operators
+#arthematic operators->(+,-,*,/)
+c= var+var1
+print(c)
+d=var-var1
+print(d)
+#relational operators->(>,<,>=,<=,==,!=)
+print(var>var1)
+print(var<var1)
+print(var==var1)
+
